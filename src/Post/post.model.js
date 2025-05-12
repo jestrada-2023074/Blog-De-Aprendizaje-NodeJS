96b500b44ee0d74bb090b355fe980aaa9698b27c
Jose Estrada <jestrada-2023074@kinal.edu.gt>
2025-05-11 20:52:45 -0600

    Update post.model.js

---
+++ src/Post/post.model.js
@@ -0,0 +1,27 @@
+import {model, Schema} from 'mongoose';
+
+const postSchema = new Schema({
+    title: {
+        type: String,
+        required: [true, 'EL titulo es requerido'],
+        trim: true,
+        maxlength: [100, 'El titulo no puede tener mas de 100 caracteres']
+    },
+    description: {
+        type: String,
+        required: [true, 'La descripcion es requerida'],
+        trim: true
+    },
+    course: {
+        type: String,
+        required: [true, ' Debe especificar el curso'],
+        enum: {
+            values: ['Tecnología', 'Practica Supervisada', 'Taller'],
+            message: '{VALUE} no es un curso valido'
+        },
+        trim: true
+    },
+    timestamp: true
+})
+
+export default model ('Post', postSchema)
