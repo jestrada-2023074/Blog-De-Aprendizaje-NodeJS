6f490ed07a3f3ea349abff71070c89385b498176
Jose Estrada <jestrada-2023074@kinal.edu.gt>
2025-05-11 20:32:32 -0600

    Create app.js

---
+++ configs/app.js
@@ -0,0 +1,30 @@
+'use strict'
+
+import express from "express"
+import morgan from 'morgan'
+import helmet from 'helmet'
+import cors from 'cors'
+
+const configs = (app) => {
+    app.use(express.json())
+    app.use(express.urlencoded({extended: false}))
+    app.use(cors())
+    app.use(helmet())
+    app.use(morgan("dev"))
+}
+
+export const initServer = () => {
+    const app = express()
+    try{
+        configs(app)
+        //routes(app)
+        app.listen(process.env.PORT)
+        console.log(`Servidor ejecutándose en el puerto ${process.env.PORT}`)
+    }catch(err){
+        console.error("Error en el Servidor:", err)
+    }
+}
+const routes = (app) => {
+    /*Se agrega Rutas de los Modelos */
+
+}
