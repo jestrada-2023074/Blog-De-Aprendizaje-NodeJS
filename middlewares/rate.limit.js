1e683fc6b16553445007e081953da20b3861d926
Jose Estrada <jestrada-2023074@kinal.edu.gt>
2025-05-11 20:32:42 -0600

    Create rate.limit.js

---
+++ middlewares/rate.limit.js
@@ -0,0 +1,11 @@
+import rateLimit from "express-rate-limit"
+
+export const limiter = rateLimit(
+    {
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 150, // Limit each IP to 100 requests per windowMs
+        message: {
+            message: 'Estás bloqueado, espera 20 minutos.'
+        }
+    }
+)
