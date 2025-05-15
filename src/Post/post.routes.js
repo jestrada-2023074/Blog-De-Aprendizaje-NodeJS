7385a13a6c3debfa5d89e34121903b7721c52712
Jose Estrada <jestrada-2023074@kinal.edu.gt>
2025-05-14 22:28:01 -0600

    Update post.routes.js

--- src/Post/post.routes.js
+++ src/Post/post.routes.js
@@ -7,12 +7,17 @@ import {
         updatePost,
         deletePost
     } from './post.controller.js'
+import { 
+        registervalidatePost,
+        updatevalidatePost,
+        getPostByID
+    } from '../../middlewares/validators.js'
 
 const api = Router()
 
-api.post('/posts', createPost)
+api.post('/posts', registervalidatePost ,createPost)
 api.get('/posts', getPosts)
-api.get('/posts/:id', getPostById)
-api.put('/posts/:id', updatePost)
-api.delete('/posts/:id', deletePost)
+api.get('/posts/:id', getPostByID, getPostById)
+api.put('/posts/:id',updatevalidatePost, getPostByID, updatePost)
+api.delete('/posts/:id',getPostByID, deletePost)
 export default api
